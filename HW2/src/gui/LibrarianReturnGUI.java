f9a7604f6de181af639172f305afce06b3741f18
RafaelKipershlak <117562693+RafaelKipershlak@users.noreply.github.com>
2025-01-03 00:53:10 +0200

    Commit2
    Created GUI for Book Return - still  in progress

---
+++ HW2/src/gui/LibrarianReturnGUI.java
@@ -0,0 +1,59 @@
+package gui;
+
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+
+import java.time.LocalDateTime;
+
+import client.ChatClient;
+import client.ClientUI;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class LibrarianReturnGUI {
+
+	
+	@FXML
+	TextField subscriberId=null;
+	
+	@FXML
+	TextField bookName=null;
+	
+	@FXML
+	Label bookArriveDate=null;
+	
+	@FXML
+	Label deadline=null;
+	
+	@FXML
+	Button sendButton=null;
+	
+	@FXML
+	Button checkButton=null;
+	
+	@FXML
+	Button exitButton=null;
+	
+//	@FXML
+//	Button send;
+	
+	
+	
+	public void sendButton(ActionEvent event) {		// method that sends information to the controller to return the book to the library
+		
+		
+	}
+	
+	
+	public void checkButton(ActionEvent event) {		// method that get information from the data the controller to return the book to the library
+		
+		ClientUI.chat.accept("select", subscriberId.getText(), j, null);
+	}
+	
+	
+	public void exitButton(ActionEvent event) {
+		System.out.println("Exit return window");
+		System.exit(0);
+	}
+}
