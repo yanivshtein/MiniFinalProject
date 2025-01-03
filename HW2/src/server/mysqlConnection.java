3ba8a8044e0a788e04fa0ebe3145b62e96e9a261
baricoh1 <baricoh1@gmail.com>
2025-01-03 17:02:53 +0200

    bari commit 3.1

--- HW2/src/server/mysqlConnection.java
+++ HW2/src/server/mysqlConnection.java
@@ -18,7 +18,7 @@ public class mysqlConnection {
 
 		try {
 			conn = DriverManager.getConnection("jdbc:mysql://localhost/hw2-shitot?serverTimezone=IST", "root",
-					"!vex123S");
+					"Aa123456");
 			System.out.println("SQL connection succeed");
 		} catch (SQLException ex) {
 			System.out.println("SQLException: " + ex.getMessage());
