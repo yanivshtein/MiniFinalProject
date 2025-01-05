38c0fc287f22511839f45da6803251bb7f95f0ae
yanivshtein <93981683+yanivshtein@users.noreply.github.com>
2025-01-06 01:04:38 +0200

    adding to .gitignore

--- HW2/src/server/mysqlConnection.java
+++ HW2/src/server/mysqlConnection.java
@@ -32,7 +32,7 @@ public class mysqlConnection {
         }
 
         try {
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/hw2-shitot?serverTimezone=IST", "root", "Sheli123");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/hw2-shitot?serverTimezone=IST", "root", "yaniv1234");
             System.out.println("SQL connection succeed");
         } catch (SQLException ex) {
             System.out.println("SQLException: " + ex.getMessage());
