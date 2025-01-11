342c59b479be7709de8dd637770d8a2782b8974d
yanivshtein <93981683+yanivshtein@users.noreply.github.com>
2025-01-11 18:03:14 +0200

    put back start for searchUI

--- HW2/src/server/mysqlConnection.java
+++ HW2/src/server/mysqlConnection.java
@@ -41,7 +41,7 @@ public class mysqlConnection {
         try {
 
 
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/hw2-shitot?serverTimezone=IST", "root", "Sheli123");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/hw2-shitot?serverTimezone=IST", "root", "yaniv1234");
             System.out.println("SQL connection succeed");
         } catch (SQLException ex) {
             System.out.println("SQLException: " + ex.getMessage());
