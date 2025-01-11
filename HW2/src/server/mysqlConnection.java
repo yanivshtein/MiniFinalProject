91ce86042a60696444e139d5a7ccd16029f2c509
baricoh1 <baricoh1@gmail.com>
2025-01-11 18:22:50 +0200

    12

--- HW2/src/server/mysqlConnection.java
+++ HW2/src/server/mysqlConnection.java
@@ -41,7 +41,7 @@ public class mysqlConnection {
         try {
 
 
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/hw2-shitot?serverTimezone=IST", "root", "yaniv1234");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/hw2-shitot?serverTimezone=IST", "root", "Aa123456");
             System.out.println("SQL connection succeed");
         } catch (SQLException ex) {
             System.out.println("SQLException: " + ex.getMessage());
