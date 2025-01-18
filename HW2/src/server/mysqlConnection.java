6c7b9f59b9bf3a97efeef2d9cc7d669d747efe1e
yanivshtein <93981683+yanivshtein@users.noreply.github.com>
2025-01-18 15:26:38 +0200

    small change instead of dialogpane label

--- HW2/src/server/mysqlConnection.java
+++ HW2/src/server/mysqlConnection.java
@@ -42,7 +42,7 @@ public class mysqlConnection {
 
 		try {
 
-			conn = DriverManager.getConnection("jdbc:mysql://localhost/hw2-shitot?serverTimezone=Asia/Jerusalem", "root", "!vex123S");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost/hw2-shitot?serverTimezone=Asia/Jerusalem", "root", "yaniv1234");
 
 
 
