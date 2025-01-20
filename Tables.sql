880684ed8c80482b030fa87ae8d5a25414c125e3
yanivshtein <93981683+yanivshtein@users.noreply.github.com>
2025-01-20 23:37:37 +0200

    updated tables

--- Tables.sql
+++ Tables.sql
@@ -31,6 +31,7 @@ CREATE TABLE `activityhistory` (
   `deadline` date DEFAULT NULL,
   `ActionID` int NOT NULL AUTO_INCREMENT,
   `reminderSent` tinyint(1) DEFAULT '0',
+  `hasReturned` int DEFAULT NULL,
   PRIMARY KEY (`ActionID`),
   KEY `SubscriberID` (`SubscriberID`),
   CONSTRAINT `activityhistory_ibfk_1` FOREIGN KEY (`SubscriberID`) REFERENCES `subscriber` (`subscriber_id`),
@@ -44,7 +45,7 @@ CREATE TABLE `activityhistory` (
 
 LOCK TABLES `activityhistory` WRITE;
 /*!40000 ALTER TABLE `activityhistory` DISABLE KEYS */;
-INSERT INTO `activityhistory` VALUES (1,'To Kill a Mockingbird','Borrow','2024-01-01','autoExtended','2024-06-29',1,0),(1,'To Kill a Mockingbird','Return','2024-01-16','1',NULL,2,0),(3,'Jane Eyre','Borrow','2024-02-02',NULL,NULL,3,0),(2,'1984','Borrow','2025-01-07','autoExtended','2025-01-28',4,1),(2,'The Hobbit','Borrow','2025-01-07','autoExtended','2025-01-15',5,0),(2,'1984','Borrow','2025-01-10','autoExtended','2025-01-16',6,1),(5,'1984','Reservation','2025-01-13',NULL,NULL,7,0),(5,'War and Peace','Reservation','2025-01-13',NULL,NULL,12,1),(2,'War and Peace','Borrow','2025-01-10',NULL,'2025-01-24',13,0),(2,'War and Peace','Return','2025-01-13','Returned on time',NULL,14,0),(5,'War and Peace','Borrow','2025-01-13','autoExtended','2025-01-16',15,1),(5,'1984','Reservation','2025-01-14',NULL,NULL,16,0),(5,'War and Peace','Reservation','2025-01-14',NULL,NULL,17,1),(5,'1984','Reservation','2025-01-14',NULL,NULL,18,0),(2,'Harry Potter and the Sorcerer\'s Stone','Borrow','2025-01-14','autoExtended','2025-02-04',19,0),(2,'The Hobbit','Return','2025-01-14','13 Days Late',NULL,21,0),(2,'War and Peace','Reservation','2025-01-18',NULL,NULL,22,0),(2,'War and Peace','Reservation','2025-01-18',NULL,NULL,24,0),(2,'War and Peace','Reservation','2025-01-18',NULL,NULL,25,0),(1,'1984','Borrow','2025-01-18',NULL,'2025-02-01',26,0),(1,'War and Peace','Reservation','2025-01-18',NULL,NULL,27,0),(2,'War and Peace','Borrow','2025-01-18',NULL,'2025-02-01',28,0),(2,'War and Peace','Return','2025-01-18','Returned on time',NULL,29,0),(1,'1984','Reservation','2025-01-18',NULL,NULL,30,0),(1,'1984','Borrow','2025-01-18',NULL,'2025-02-01',31,0),(1,'1984','Reservation','2025-01-18',NULL,NULL,32,0);
+INSERT INTO `activityhistory` VALUES (1,'To Kill a Mockingbird','Borrow','2024-01-01','autoExtended','2024-05-11',1,0,1),(1,'To Kill a Mockingbird','Return','2024-01-16','1',NULL,2,0,NULL),(3,'Jane Eyre','Borrow','2024-02-02',NULL,NULL,3,0,NULL),(2,'1984','Borrow','2025-01-07','autoExtended','2025-01-28',4,1,0),(2,'The Hobbit','Borrow','2025-01-07','autoExtended','2025-01-15',5,0,NULL),(2,'1984','Borrow','2025-01-10','autoExtended','2025-01-16',6,1,NULL),(5,'1984','Reservation','2025-01-13',NULL,NULL,7,0,NULL),(5,'War and Peace','Reservation','2025-01-13',NULL,NULL,12,1,NULL),(2,'War and Peace','Borrow','2025-01-10',NULL,'2025-01-24',13,0,NULL),(2,'War and Peace','Return','2025-01-13','Returned on time',NULL,14,0,NULL),(5,'War and Peace','Borrow','2025-01-13','autoExtended','2025-01-16',15,1,NULL),(5,'1984','Reservation','2025-01-14',NULL,NULL,16,0,NULL),(5,'War and Peace','Reservation','2025-01-14',NULL,NULL,17,1,NULL),(5,'1984','Reservation','2025-01-14',NULL,NULL,18,0,NULL),(2,'Harry Potter and the Sorcerer\'s Stone','Borrow','2025-01-14','autoExtended','2025-02-04',19,0,NULL),(2,'The Hobbit','Return','2025-01-14','13 Days Late',NULL,21,0,NULL),(2,'War and Peace','Reservation','2025-01-18',NULL,NULL,22,0,NULL),(2,'War and Peace','Reservation','2025-01-18',NULL,NULL,24,0,NULL),(2,'War and Peace','Reservation','2025-01-18',NULL,NULL,25,0,NULL),(1,'1984','Borrow','2025-01-18',NULL,'2025-02-01',26,0,NULL),(1,'War and Peace','Reservation','2025-01-18',NULL,NULL,27,0,NULL),(2,'War and Peace','Borrow','2025-01-18',NULL,'2025-02-01',28,0,NULL),(2,'War and Peace','Return','2025-01-18','Returned on time',NULL,29,0,NULL),(1,'1984','Reservation','2025-01-18',NULL,NULL,30,0,NULL),(1,'1984','Borrow','2025-01-18',NULL,'2025-02-01',31,0,NULL),(1,'1984','Reservation','2025-01-18',NULL,NULL,32,0,NULL);
 /*!40000 ALTER TABLE `activityhistory` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -76,6 +77,31 @@ LOCK TABLES `books` WRITE;
 /*!40000 ALTER TABLE `books` DISABLE KEYS */;
 INSERT INTO `books` VALUES ('1984','0','15','Science Fiction',NULL,'George Orwell',1,'A'),('Harry Potter and the Sorcerer\'s Stone','9','25','	Fantasy',NULL,'J. K. Rowling',2,'A'),('Moby Dick','3','6','	Adventure',NULL,'Herman Melville',3,'A'),('Pride and Prejudice','6','12','Romantic Novel',NULL,'Jane Austen',4,'A'),('The Catcher in the Rye','4','8','Fiction',NULL,'J.D. Salinger',5,'A'),('The Great Gatsby','8','20','Drama',NULL,'	F. Scott Fitzgerald',6,'A'),('The Hobbit','11','18','	Fantasy',NULL,'J. R. R. Tolkien',7,'A'),('The Lord of the Rings','7','14','	Fantasy',NULL,'J. R. R. Tolkien',8,'A'),('To Kill a Mockingbird','5','10','Drama',NULL,'	Harper Lee',9,'A'),('War and Peace','1','5','Drama',NULL,'	Leo Tolstoy',10,'A');
 /*!40000 ALTER TABLE `books` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `frozen_subs`
+--
+
+DROP TABLE IF EXISTS `frozen_subs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `frozen_subs` (
+  `subscriber_id` int NOT NULL,
+  `start_date` date DEFAULT NULL,
+  `finish_date` date DEFAULT NULL,
+  PRIMARY KEY (`subscriber_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `frozen_subs`
+--
+
+LOCK TABLES `frozen_subs` WRITE;
+/*!40000 ALTER TABLE `frozen_subs` DISABLE KEYS */;
+INSERT INTO `frozen_subs` VALUES (2,'2025-01-05','2025-02-01');
+/*!40000 ALTER TABLE `frozen_subs` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -252,7 +278,7 @@ CREATE TABLE `subscriber` (
 
 LOCK TABLES `subscriber` WRITE;
 /*!40000 ALTER TABLE `subscriber` DISABLE KEYS */;
-INSERT INTO `subscriber` VALUES (1,'dor','1','1','Active','1','2024-08-16'),(2,'John Doe','123-456-7890','john.doe@example.com','Frozen','123','2025-06-11'),(3,'Jane Smith','987-654-3210','jane.smith@example.com','active',NULL,NULL),(4,'Alice Johnson','555-666-7777','alice.johnson@example.com','active',NULL,NULL),(5,'1','1','2','active','3',NULL),(14069,'beri','','','active','1',NULL),(48830,'1231','123456','@','active','123',NULL);
+INSERT INTO `subscriber` VALUES (1,'dor','1','1','Active','1','2024-08-16'),(2,'John Doe','123-456-7890','john.doe@example.com','Frozen','123','2024-01-11'),(3,'Jane Smith','987-654-3210','jane.smith@example.com','active',NULL,NULL),(4,'Alice Johnson','555-666-7777','alice.johnson@example.com','active',NULL,NULL),(5,'1','1','2','active','3',NULL),(14069,'beri','','','active','1',NULL),(48830,'1231','123456','@','active','123',NULL);
 /*!40000 ALTER TABLE `subscriber` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -265,4 +291,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-01-19 19:36:57
+-- Dump completed on 2025-01-20 23:31:45
