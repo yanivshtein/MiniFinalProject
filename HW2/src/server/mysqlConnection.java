e07f9948ddcb7b22c6884212cc90d18b9f421529
Sheli-Zisman <sheli.zisman1@gmail.com>
2025-01-23 16:34:02 +0200

    gfmk

--- HW2/src/server/mysqlConnection.java
+++ HW2/src/server/mysqlConnection.java
@@ -58,7 +58,7 @@ public class mysqlConnection {
         }
 
         try {
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/hw2-shitot?serverTimezone=Asia/Jerusalem", "root", "yaniv1234");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/hw2-shitot?serverTimezone=Asia/Jerusalem", "root", "Sheli123");
             System.out.println("SQL connection succeed");
         } catch (SQLException ex) {
             System.out.println("SQLException: " + ex.getMessage());
