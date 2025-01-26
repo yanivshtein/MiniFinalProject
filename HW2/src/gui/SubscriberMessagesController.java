7881f7cd4102aa9871296f5ee4657d713bdf91a6
RafaelKipershlak <117562693+RafaelKipershlak@users.noreply.github.com>
2025-01-26 23:07:29 +0200

    Commit Forgot to delete Import

--- HW2/src/gui/SubscriberMessagesController.java
+++ HW2/src/gui/SubscriberMessagesController.java
@@ -10,7 +10,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.DialogPane;
 import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 
